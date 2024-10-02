993b487964116981de0c5bb9d068d396f2910acf
Sema Bekdemir <ecesemadimen96@gmail.com>
2024-10-02 23:48:01 +0300

    basket silindi

--- app/products/page.jsx
+++ app/products/page.jsx
@@ -6,23 +6,43 @@ export default async function Products() {
   );
   console.log(products);
 
+  function renderStars(rating) {
+    const totalStars = 5;
+    const stars = [];
+
+    for (let i = 1; i <= totalStars; i++) {
+      if (rating >= i) {
+        stars.push(<span key={i} className="fa fa-star checked"></span>);
+      } else if (rating >= i - 0.5) {
+        stars.push(<span key={i} className="fa fa-star-half-o checked"></span>);
+      } else {
+        stars.push(<span key={i} className="fa fa-star-o"></span>);
+      }
+    }
+
+    return stars;
+  }
+
   return (
     <div className="productContainer">
       <ul className="productsCard">
         {products.map((x) => (
-          <li className="product" key={x.id}>
-            <img src={x.thumbnail} alt="" />
-            <div className="productTitle">
-              <h3>{x.title}</h3>
-            </div>
-            <div className="product-rating">
-              <p>{x.rating}</p>
-            </div>
-            <div className="productPrice">
-              <h4>
-                <span>$</span>
-                {x.price}
-              </h4>
+          <li className="product degiscek" key={x.id}>
+            <div>
+              <img src={x.thumbnail} alt="" />
+              <div className="productTitle">
+                <h3>{x.title}</h3>
+              </div>
+              <div className="product-rating">
+                <p className="stars">{renderStars(x.rating)}</p>
+                <p>{x.rating}</p>
+              </div>
+              <div className="productPrice">
+                <h4>
+                  <span>$</span>
+                  {x.price}
+                </h4>
+              </div>
             </div>
             <Link className="detail" href={`/products/${x.id}`}>
               Click to see comments
